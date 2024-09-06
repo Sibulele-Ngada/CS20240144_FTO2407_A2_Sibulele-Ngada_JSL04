f406b6e3f6c1032881ebb23a7d57afbe1f7a864a
Sibulele Ngada <sibulelengada@gmail.com>
2024-09-06 10:14:20 +0200

    Complete challenge 5, ternary operator to check time

--- index.js
+++ index.js
@@ -37,17 +37,5 @@ console.log(currentTemp <= 5 ? "Chilled to perfection!":"Needs a cooler!");
 // otherwise, print "Better stick to water."
 
 // Define the current hour in 24-hour format
-let currentHour = 22; 
-
-// Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
-// If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
-// Otherwise, print "Better stick to water."
-
-
-
-
-
-
-// In each of these challenges, the ternary operator is used to decide between two options based on a condition, 
-// demonstrating its utility for concise conditional logic. Remember that while ternaries can make the code more succinct, 
-// they should be used judiciously to maintain readability, especially for more complex conditions.
+let currentHour = 2; 
+console.log(currentHour >= 7 ? "Unleash the beast!":"Better stick to water.");
